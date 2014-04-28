241d9fabb3ba91865c7aed09d83f670efd9a5484
Kartik <kartik@FlynnRider.(none)>
2014-04-28 18:53:18 +0530

    Created the Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labweb::Application.config.secret_key_base = '7e3e923b9d9a4d009d3fafad38f35dbbb2b3972a7c0f9d6b5a474cf9e450ad5232b3c3b7a395311e6974b151d88f62dfa00e3cd3a4172e6defb0fb6946a88d9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labweb::Application.config.session_store :cookie_store, key: '_Labweb_session'
